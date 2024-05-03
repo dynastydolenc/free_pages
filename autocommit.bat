0a6a7f7bf8c9f15092a84d03442266754a0e4d94
dynastydolenc <ljuvncuser@corp.dynastypower.com>
2024-05-03 16:19:05 +0200

    autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%

---
+++ autocommit.bat
@@ -0,0 +1,12 @@
+cd C:\Users\ljuvncuser\repos/energy_market
+
+C:\Users\ljuvncuser\AppData\Local\anaconda3\python -m src.visualization.remitPlot %*
+
+
+
+
+cd C:\Users\ljuvncuser\repos\free_pages
+git add --all
+git commit -m "autoCommit %date:~-4%%date:~3,2%%date:~0,2%.%time:~0,2%%time:~3,2%%time:~6,2%"
+git push
+exit
